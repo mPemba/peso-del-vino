97fd6e53fcaace69c79be4027378f8209780d98f
mPemba <michael.j.say@gmail.com>
2015-08-19 18:33:42 -0600

    details

--- public/controllers/loggedInCtrl.js
+++ public/controllers/loggedInCtrl.js
@@ -25,39 +25,39 @@ app.controller('loggedInCtrl', function($scope, $rootScope, $location, loggedInS
 	}
 
 	$('.startButton').on('click', function() {
-		$('.homeHeadline').fadeOut(500).addClass('hide');
-		$('.homeSubHeadline').fadeOut(500).addClass('hide');
-		$('.startButton').fadeOut(500).addClass('hide');
+		$('.homeHeadline').fadeOut(400).addClass('hide');
+		$('.homeSubHeadline').fadeOut(400).addClass('hide');
+		$('.startButton').fadeOut(400).addClass('hide');
 		$('.homeLink').fadeOut(100).addClass('hide');
-		$('.startOver').fadeIn(500).removeClass('hide');
+		$('.startOver').fadeIn(400).removeClass('hide');
 		$('.navbar').slideDown("slow").removeClass('hide');
 		$('.navbar-link').slideDown("slow").removeClass('hide');
 	});
 
 	$('.startOver').on('click', function() {
 		$('.startOver').fadeOut(100).addClass('hide');
-		$('.homeLink').fadeIn(500).removeClass('hide');
-		$('.navbar').fadeOut(500).addClass('hide');
-		$('.navbar-link').fadeOut(500).addClass('hide');
-		$('.cookingStyleNavbar').fadeOut(500).addClass('hide');
-		$('.cookingStyleBtn').fadeOut(500).addClass('hide');
-		$('.homeHeadline').fadeIn(500).removeClass('hide');
-		$('.homeSubHeadline').fadeIn(500).removeClass('hide');
-		$('.startButton').fadeIn(500).removeClass('hide');
+		$('.homeLink').fadeIn(400).removeClass('hide');
+		$('.navbar').fadeOut(400).addClass('hide');
+		$('.navbar-link').fadeOut(400).addClass('hide');
+		$('.cookingStyleNavbar').fadeOut(400).addClass('hide');
+		$('.cookingStyleBtn').fadeOut(400).addClass('hide');
+		$('.homeHeadline').fadeIn(400).removeClass('hide');
+		$('.homeSubHeadline').fadeIn(400).removeClass('hide');
+		$('.startButton').fadeIn(400).removeClass('hide');
 	});
 
 	$('.main-course-link').on('click', function() {
-		$('.navbar').fadeOut(500).addClass("hide");
-		$('.navbar-link').fadeOut(500).addClass("hide");
-		$('.cookingStyleNavbar').fadeIn(500);
-		$('.cookingStyleBtn').fadeIn(500);
+		$('.navbar').fadeOut(800).addClass("hide");
+		$('.navbar-link').fadeOut(400).addClass("hide");
+		$('.cookingStyleNavbar').fadeIn(400);
+		$('.cookingStyleBtn').fadeIn(400);
 	});
 
 	$('.homeLink').on('click', function() {
-		$('.navbar').fadeOut(500).addClass('hide');
-		$('.navbar-link').fadeOut(500).addClass('hide');
-		$('.homeHeadline').fadeIn(500).removeClass('hide');
-		$('.homeSubHeadline').fadeIn(500).removeClass('hide');
-		$('.startButton').fadeIn(500).removeClass('hide');
+		$('.navbar').fadeOut(400).addClass('hide');
+		$('.navbar-link').fadeOut(400).addClass('hide');
+		$('.homeHeadline').fadeIn(400).removeClass('hide');
+		$('.homeSubHeadline').fadeIn(400).removeClass('hide');
+		$('.startButton').fadeIn(400).removeClass('hide');
 	});
 });
